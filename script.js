6068869d65544d4e6cf075f2ee810b9fb7bec38b
SurenderHarsha <battusurender96@gmail.com>
2017-01-03 22:20:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+   $('div').mouseenter(function() {
+       $(this).animate({
+           height: '+=10px'
+           
+       });
+   });
+   $('div').mouseleave(function() {
+       $(this).animate({
+           height: '-=10px'
+           
+       }); 
+   });
+   $('div').click(function() {
+       $(this).toggle(1000);
+   }); 
+});
